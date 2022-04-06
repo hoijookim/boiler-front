e91eca98e2bfbc74c696cad5c203a7b7d4b58d12
hoijoo <hoijookim@gmail.com>
2022-04-06 09:27:11 +0900

    Update: app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,6 +17,25 @@ const App = ({ Component, pageProps }: AppProps) => {
         <Head>
           <meta charSet="utf-8" />
           <title>App</title>
+          <style>
+          {`html, body {
+              margin: 0;
+              padding: 0;
+              overflow: initial !important;
+            }
+            body {
+                height:100%
+                font-size: 15px;
+                line-height: 1.46668;
+                font-weight: 400;
+                font-variant-ligatures: common-ligatures;
+                -moz-osx-font-smoothing: grayscale;
+                -webkit-font-smoothing: antialiased;
+            }
+            * {
+                box-sizing: border-box;
+            }`}
+        </style>
         </Head>
         <Component {...pageProps} />
         <ReactQueryDevtools initialIsOpen={false} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,6 +29,10 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+            rel="stylesheet"
+          />
         <body>
           <Main />
           <NextScript />
